3463c4532fed99e8637d52f9c9e75a93b63341f4
Kate Peterson <61802919+kitteap@users.noreply.github.com>
2020-03-24 13:29:47 -0700

    put my stuff in, made a fun headache inducing game :)

--- 4-sound /sketch.js
+++ 4-sound /sketch.js
@@ -1,18 +1,19 @@
+
 //create an empty array called balls
 let balls = [];
 
 //create a variable to hold your avatar
 let me;
-
 let mySound;
 
 function preload() {
   soundFormats('mp3', 'ogg');
-  mySound = loadSound('boing1.mp3');
+  mySound = loadSound('squelchysplat.mp3');
 }
 
 function setup() {
   createCanvas(500, 400);
+  angleMode(DEGREES);
 
   //make one avatar called me
   me = new Avatar(width/2, 300, 3);
@@ -20,7 +21,11 @@ function setup() {
 }
 
 function draw(){
-	background(220);
+if (frameCount % 10 == 0){
+  background(random(0,255),random(0,255),random(0,255));
+}
+  
+
 
   me.drawMe();
   me.moveMe();
@@ -50,16 +55,25 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
+    		stroke(0,0,0);
         strokeWeight(3);
-    		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+    		fill(255,255,255);
+		    ellipse(this.x, this.y, 25, 25); //head
+        ellipse(this.x+5, this.y+3, 15,13); //mouth
+        ellipse(this.x-5, this.y-3, 3, 3); //eye
+        line(this.x,this.y+12, this.x, this.y+40); //torso
+        line(this.x, this.y+40, this.x-20, this.y+60); //left leg
+        ellipse(this.x-15, this.y+60, 10, 5); //left shoe
+        line(this.x, this.y+40, this.x+10, this.y+50); //right leg
+        line(this.x+10, this.y+50, this.x+5, this.y+60); //right leg
+        ellipse(this.x+10, this.y+60, 10, 5); //right shoe
+        line(this.x, this.y+15, this.x-10, this.y+25); //top arm
+        line(this.x-10, this.y+25, this.x+10, this.y+35); //bottom arm
+        fill(51,204,255);
+        triangle(this.x-10,this.y-8,this.x+8,this.y-11,this.x-5,this.y-32);
+        noFill();
+        ellipse(this.x-6,this.y-35,5,5);
+
 	}
 
 	moveMe(){
@@ -69,6 +83,14 @@ class Avatar {
 
     if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
         this.y += this.speed;
+    }
+
+    if (keyIsDown(LEFT_ARROW)) { // if you hold the left arrow, move down by speed
+        this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the left arrow, move down by speed
+        this.x += this.speed;
     }
 	}
 
@@ -85,16 +107,26 @@ class Ball {
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
 		this.x = x;
-    		this.y = y;
-        	this.speed = speed;
+    this.y = y;
+    this.speed = speed;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-    		stroke(0);
-        	strokeWeight(1);
-    		fill("red");
-		ellipse(this.x,this.y,10,10);
+    	stroke(0,55,185);
+      strokeWeight(2);
+    	fill(128, 170, 255);
+      arc(this.x, this.y, 30, 50, 180, 0);
+      strokeWeight(3);
+      bezier(this.x-10,this.y+5,this.x-20,this.y+15,this.x-5,this.y+25,this.x-10,this.y+35);
+      bezier(this.x,this.y+5,this.x-10,this.y+15,this.x+5,this.y+25,this.x,this.y+35);
+      bezier(this.x+10,this.y+5,this.x,this.y+15,this.x+15,this.y+25,this.x+10,this.y+35);
+
+      stroke(0,204,102);
+      strokeWeight(1.5);
+      bezier(this.x-5,this.y+2,this.x-15,this.y+12,this.x,this.y+22,this.x-5,this.y+32);
+      bezier(this.x+5,this.y+2,this.x-5,this.y+12,this.x+10,this.y+22,this.x+5,this.y+32);
+
 	}
 
 	//update the location of the ball, so it moves across the screen
@@ -103,12 +135,12 @@ class Ball {
 		this.y = this.y+.5;
 	}
 
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-            mySound.setVolume(0.1);
-            mySound.play();
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
+  bounceBall(){
+      if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+          this.speed = -this.speed;
+          mySound.setVolume(0.1);
+          mySound.play();
     		}
   	}
 
