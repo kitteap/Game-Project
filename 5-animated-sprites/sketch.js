aed28e4de328a4492e04917616a4553a5626af7f
Kate Peterson <61802919+kitteap@users.noreply.github.com>
2020-03-27 13:38:35 -0700

    added my sprite and started to work on how to move it

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -10,19 +10,44 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  ghost = loadAnimation('sprites/thing01.png', 'sprites/thing20.png');
 
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(200, 255, 255);
+  background(255, 255, 255);
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
+  animation(ghost, 300, 300);
 
+}
+
+
+
+
+//trying to figure out how to move it below
+this.x = x;
+this.y = y;
+this.speed = speed;
+
+
+if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+   this.y -= this.speed;
+}
+
+if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+    this.y += this.speed;
+}
+
+if (keyIsDown(LEFT_ARROW)) { // if you hold the left arrow, move down by speed
+    this.x -= this.speed;
+}
+
+if (keyIsDown(RIGHT_ARROW)) { // if you hold the left arrow, move down by speed
+    this.x += this.speed;
 }
